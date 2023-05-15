7a97168b34fd1494555a82b5b2bc2a4bbdfa7cbf
Kaditcuy <philipukchimaobi@gmail.com>
2023-05-15 04:08:36 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,46 @@
 #!/usr/bin/python3
-"""reads stdin line by line and computes metrics:"""
 
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+def print_stats(file_size, status_codes):
+    """Prints the statistics."""
+    print("File size: {}".format(file_size))
+    for key in sorted(status_codes):
+        if status_codes[key] != 0:
+            print("{}: {}".format(key, status_codes[key]))
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
 
-except Exception as err:
-    pass
+if __name__ == "__main__":
+    count = 0
+    file_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0,
+                    "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    try:
+        # read stdin line by line
+        for line in sys.stdin:
+            # increment line count
+            count += 1
+            # split line into list of strings
+            split_line = line.split()
+            # validation for correct format of line (2 <= elements <= 9)
+            if len(split_line) >= 2 and len(split_line) <= 9:
+                # check that the file size is a valid integer
+                if split_line[-1].isdigit():
+                    # add the file size to the total
+                    file_size += int(split_line[-1])
+                # check that the status code is valid
+                if split_line[-2] in status_codes:
+                    # increment the status code count
+                    status_codes[split_line[-2]] += 1
+            # print stats every 10 lines
+            if count % 10 == 0:
+                print_stats(file_size, status_codes)
+        # print final stats at the end
+        print_stats(file_size, status_codes)
+
+    except KeyboardInterrupt:
+        # print final stats if keyboard interrupt
+        print_stats(file_size, status_codes)
+        raise
